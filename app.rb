9c3f0c428e5af8fab171b929c15112161bf4e4ca
Manu Kapoor <manu@manukapoor.net>
2011-02-02 20:47:31 -0800

    testing without couchdb

--- app.rb
+++ app.rb
@@ -14,23 +14,23 @@ end
 
 get_or_post '/' do
   
-  =begin
-  if params.empty? == false then
-    if params[:sawmessage].empty? == false then
-      RestClient.post 'http://127.0.0.1:5984/sawtest', {'body'=>"#{params[:sawmessage]}", 'userid'=>"999", 'time'=>[2,2,2011]}.to_json, :content_type => :json, :accept => :json
-    end
-  else
-    puts "empty message motherfucker"
-  end
   
-  data = RestClient.get 'http://127.0.0.1:5984/sawtest/_design/example/_view/viewall'
-  puts "yeah"
-  @result = JSON.parse(data)['rows']
+ # if params.empty? == false then
+#    if params[:sawmessage].empty? == false then
+#      RestClient.post 'http://127.0.0.1:5984/sawtest', {'body'=>"#{params[:sawmessage]}", 'userid'=>"999", 'time'=>[2,2,2011]}.to_json, :content_type => :json, :accept => :json
+#    end
+#  else
+#    puts "empty message motherfucker"
+#  end
+  
+#  data = RestClient.get 'http://127.0.0.1:5984/sawtest/_design/example/_view/viewall'
+#  puts "yeah"
+#  @result = JSON.parse(data)['rows']
    #do |row|
   #  puts row['key'] << row['value']
   #end
-  erb :index
-  =end
-  puts "hello"
+#  erb :index
+  
+  "Hello Heroku"
 end
 

bc098989ba5ec1b200c57e2025e82684e0189649
Manu Kapoor <manu@manukapoor.net>
2011-02-02 21:27:29 -0800

    fixed db access issues

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'rest_client'
 
 
-DB = 'http://127.0.0.1:5984/sawtest'
+DB = "#{ENV['CLOUDANT_URL']}/sawtest"
 
 def get_or_post(path, opts={}, &block)
   get(path, opts, &block)
@@ -17,13 +17,13 @@ get_or_post '/' do
   
   if params.empty? == false then
     if params[:sawmessage].empty? == false then
-      RestClient.post 'http://127.0.0.1:5984/sawtest', {'body'=>"#{params[:sawmessage]}", 'userid'=>"999", 'time'=>[2,2,2011]}.to_json, :content_type => :json, :accept => :json
+      RestClient.post "#{DB}", {'body'=>"#{params[:sawmessage]}", 'userid'=>"999", 'time'=>[2,2,2011]}.to_json, :content_type => :json, :accept => :json
     end
   else
     puts "empty message motherfucker"
   end
   
-  data = RestClient.get 'http://127.0.0.1:5984/sawtest/_design/example/_view/viewall'
+  data = RestClient.get "#{DB}/_design/example/_view/viewall"
   puts "yeah"
   @result = JSON.parse(data)['rows']
    #do |row|
